490acf4141c5fe40a37200a735ce1f7b0204937d
catdintran <trancatdin86@yahoo.com>
2017-03-12 12:20:07 -0400

    refine convert

--- pdf_to_html_pdfminer6.py
+++ pdf_to_html_pdfminer6.py
@@ -1,78 +1,68 @@
 # PDF to HTML (or TXT - commented out) formats with pdfminer.six
+import os, platform
+import subprocess
+import shutil
 
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-#from pdfminer.converter import TextConverter
-from pdfminer.converter import HTMLConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO, BytesIO
-#import regex
-import csv, re, os, sys
-from timeit import default_timer as timer
-import datetime as dt
-#import multiprocessing
 
-def readPDF(pdfFile):
-    rsrcmgr = PDFResourceManager()
-    #retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    #device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+def changeNameToCountry():
+    # clear/retrieve txt file
+    txt_list = [txt for txt in os.listdir(output) if txt.endswith('.txt')]
+    newOutput = os.getcwd() + '\\txt\\'
+    # iterate and process txt_list to new txt folder
+    for f in txt_list:
+        fp = open(output + f)
+        fileName = ''
+        docDate = ''
+        for i, line in enumerate(fp):
+            # retrieve doc date from 1st line
+            if i == 0:
+                # print(line.replace('\n', ''))
+                docDate = line.replace('\n', '')
+                # retrieve doc title from range(2,4)
+            if i in range(2, 4) and 'IMF' not in line:
+                # make sure line is not an empty line
+                if line.strip():
+                    fileName = line.replace('\n', '') + '--' + docDate
+            if i > 4:
+                break
+        fp.close()
 
-    output = BytesIO()
-    print("stage1")
-    converter = HTMLConverter(rsrcmgr, output, codec=codec, laparams=LAParams())
+        if not os.path.exists(newOutput):
+            os.makedirs(newOutput)
 
-    interpreter = PDFPageInterpreter(rsrcmgr, converter)
-    print("stage2")
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos=set()
-    for page in PDFPage.get_pages(pdfFile, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-    converter.close()
-    print("stage3")
-    #textstr = retstr.getvalue()
-    convertedPDF = output.getvalue()
-    print("stage4")
-    #retstr.close()
-    output.close()
-    #device.close()
-    return convertedPDF
-    #return textstr
+        shutil.copy(output + f, newOutput + fileName + '.txt')
+
+
+def processPdfToTxt():
+    # check platform system Windows/Macs
+    xpdfPath = ''
+    if platform.system() == 'Windows':
+        xpdfPath = os.getcwd() + '\\xpdfbin-win-3.04\\bin64\\'
+    else:
+        xpdfPath = os.getcwd() + '\\xpdfbin-mac-3.04\\bin64\\'
+
+    # store .pdf files from input
+    doc_list = [pdf for pdf in os.listdir(input) if pdf.endswith('.pdf')]
+    # iterate and process doc_list to txt file
+    for doc in doc_list:
+        subprocess.call('pdftotext ' + input + doc + ' ' + output + doc.replace('.pdf', '.txt'), cwd=xpdfPath,
+                        shell=True)
 
 
 if __name__ == "__main__":
     # pdf files dir
-    path1 = "U:\\***\\cap-articles_download\\"
+    input = os.getcwd() + '\\pdf\\'
     # final dir after converted
-    path2 = "U:\\***\\cap-articles_download\\html\\"
-    
-    # create html folder if it is not already existed
-    if not os.path.exists(path2):        
-        os.makedirs(path2)
-        
-    # placegholder for .pdf files
-    doc_list = []
-    
-    # iterate through .pdf files from path1
-    for pdfFile in os.listdir(path1):
-        if pdfFile.endswith('.pdf'):
-            doc_list.append(pdfFile)
-    
-    # iterate and process doc_list to html file
-    for doc in doc_list:
-        print("working with doc: {}".format(doc))
-        fileHTML = doc.replace('pdf','html')
-        pdfFile = path1 + doc
-        
-        scrape = open(pdfFile, 'rb')
-        pdfFile = BytesIO(scrape.read())
-        convertedPDF = readPDF(pdfFile)
-        htmlFile = path2 + fileHTML
-        fileConverted = open(htmlFile, "wb")
-        fileConverted.write(convertedPDF)
-        fileConverted.close()
-        print("Done with file {}".format(fileHTML))
-        
+    output = os.getcwd() + '\\tmp\\'
+
+    # check/create if output folder exist
+    if not os.path.exists(output):
+        os.makedirs(output)
+
+    processPdfToTxt()
+
+    #######################################
+    # format txt file name to country name
+    #######################################
+
+    changeNameToCountry()
